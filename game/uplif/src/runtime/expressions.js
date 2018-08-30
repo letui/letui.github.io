72ae23d7c5b0f0775dd8f0ccad699f091192d459
liyanbin <liyanbin@weishangtech.com>
2018-08-30 11:28:08 +0800

    game

---
+++ game/uplif/src/runtime/expressions.js
@@ -0,0 +1,30 @@
+var ls;
+(function (ls) {
+	ls.MainScene = function() {
+		return {
+			"ls.random()*120%2B100": function() { return ls.random()*120+100 },
+			"420%2Bls.random()*100": function() { return 420+ls.random()*100 },
+			"AISprite2944": function() { return AISprite2944 },
+			"ls.random()*3%2B6": function() { return ls.random()*3+6 },
+			"ls.random()*6%2B9": function() { return ls.random()*6+9 },
+			"%22score_count%22": function() { return "score_count" },
+			"AISprite2926": function() { return AISprite2926 },
+			"ls.random()*300%2Bls.random()*300%2B400": function() { return ls.random()*300+ls.random()*300+400 },
+			"AISprite2932": function() { return AISprite2932 },
+			"20%2Bls.random()*10": function() { return 20+ls.random()*10 },
+			"AISprite2929": function() { return AISprite2929 },
+			"player1run": function() { return player1run },
+			"%22inground%22": function() { return "inground" },
+			"ls.random()*100%2B350": function() { return ls.random()*100+350 },
+			"AISprite2938": function() { return AISprite2938 },
+			"700%2Bls.random()*700": function() { return 700+ls.random()*700 },
+			"ls.random()*90": function() { return ls.random()*90 },
+			"ls.random()*10%2B6": function() { return ls.random()*10+6 },
+			"260%2Bls.random()*20": function() { return 260+ls.random()*20 },
+			"AISprite2935": function() { return AISprite2935 },
+			"ls.random()*9%2Bls.random()*5": function() { return ls.random()*9+ls.random()*5 },
+			"800%2Bls.random()*100": function() { return 800+ls.random()*100 },
+			"System.score_count": function() { return System.score_count }
+		}
+	};
+})(ls || (ls = {}));
