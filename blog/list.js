f756282884763611e5a7ae6e40078b1e04c39587
liyanbin <liyanbin@weishangtech.com>
2018-08-31 16:21:47 +0800

    ..

--- blog/list.js
+++ blog/list.js
@@ -1,6 +1,6 @@
 var $blog={
 total:1,
-last:3,
+last:5,
 size:9,
 count:0
 }
@@ -28,6 +28,29 @@ for(i=$blog.last;i>0;i--){
 			}
 $(".item[tpl=blog]").remove();
 }
+function initBlogPages(){
+	for(i=$blog.last;i>0;i--){
+					$blog.count++;
+					if($blog.count>$blog.size)break;
+					$.ajax({
+						url:"blog/"+i+"/"+"text.html?"+Math.random(),async:false,
+						success:function(html){
+							var txArray=html.split("#");
+							var tmpl=$(".item[tpl=blog]").html();
+							var item=$(".item[tpl=blog]").clone();
+							//tmpl=tmpl.replace("#tpl_image",i+"/img.png?"+Math.random());
+							tmpl=tmpl.replace("#tpl_title",txArray[0]);
+							tmpl=tmpl.replace("#tpl_id",i);
+							tmpl=tmpl.replace("#tpl_label","分类:"+txArray[1]);
+							tmpl=tmpl.replace("#tpl_summary",txArray[2].substr(0,140));
+							item.removeAttr("tpl");
+							item.html(tmpl);
+							$("#tpl_blog_list").append(item);
+						}
+					});
+				}
+	$(".item[tpl=blog]").remove();
+	}
 
 function initArticle(id){
 	$.ajax({
